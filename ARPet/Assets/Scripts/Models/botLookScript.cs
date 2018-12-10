3b203500a124fb6a3e277167816f312c27ce8351
OPISTO\leo.virolainen <virolainen.leo@gmail.com>
2018-12-10 14:29:18 +0200

    LookScript - ARbot seuraa haluttua esinettä silmillään sivusuunnassa.

---
+++ ARPet/Assets/Scripts/Models/botLookScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botLookScript : MonoBehaviour {
+    public Transform target;
+    public Transform eye;
+    public float maxX;
+    public float xFactor = 3;
+    Vector3 origPos;
+
+	void Start () {
+        origPos = eye.position;
+	}
+	
+
+	void Update () {
+        //Debug.DrawLine(target.position, transform.position);
+       var localDir = transform.InverseTransformVector(target.position - transform.position);
+        localDir.Normalize();
+        eye.position = origPos + eye.right * xFactor * localDir.x;
+    }
+}
