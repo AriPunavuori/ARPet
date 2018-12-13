19fa2ae8b78c5854c9b1bd05cecb624a61509304
Ari Punavuori <ari.punavuori@gmail.com>
2018-12-13 13:15:06 +0200

    Grabber tuning

--- ARPet/Assets/Scripts/BoxScript.cs
+++ ARPet/Assets/Scripts/BoxScript.cs
@@ -9,15 +9,26 @@ public class BoxScript : MonoBehaviour,IDraggable {
     IDragger dragger;
     Vector3 oldPos;
     Vector3 newPos;
+    Vector3 fakeUp;
+    Vector3 fakeForward;
     public bool isMoving;
     float movingThreshold = 0.1f;
     Collider scareCol;
     Quaternion dragStartRot;
     Quaternion draggerStartRot;
-
+    public float maxRotSpeed;
     Rigidbody rb;
 
     public void OnDragStart(IDragger dragger, Quaternion draggerRotation) {
+        var rotCheck = new Vector3[] { transform.right, -transform.right, transform.up, -transform.up, transform.forward, -transform.forward };
+        for (int i = 0 ; i<rotCheck.Length ; i++) {
+            if(rotCheck[i].y > fakeUp.y) {
+                fakeUp = rotCheck[i];
+            }
+            if(rotCheck[i].z > fakeForward.z) {
+                fakeForward = rotCheck[i];
+            }
+        }
         this.dragger = dragger;
         dragStartRot = transform.rotation;
         draggerStartRot = draggerRotation;
@@ -27,6 +38,7 @@ public class BoxScript : MonoBehaviour,IDraggable {
     public void OnDragEnd() {
         dragger = null;
         rb.isKinematic = false;
+        fakeUp = Vector3.zero;
     }
 
     public void OnDragContinue(Vector3 target) {
@@ -36,8 +48,10 @@ public class BoxScript : MonoBehaviour,IDraggable {
     public void OnDragContinue(Vector3 target, Quaternion draggerRotation) {
         rb.velocity = Vector3.zero;
         var targetRot = draggerRotation * Quaternion.Inverse(draggerStartRot) * dragStartRot;
+
+        targetRot = Quaternion.LookRotation(Vector3.ProjectOnPlane(targetRot * Vector3.forward,Vector3.up), Vector3.up);
         rb.MovePosition(target);
-        rb.MoveRotation(targetRot);
+        rb.MoveRotation(Quaternion.RotateTowards(transform.rotation,targetRot,maxRotSpeed * Time.deltaTime)) ;
     }
 
     public bool IsCurrentlyDraggable() {
