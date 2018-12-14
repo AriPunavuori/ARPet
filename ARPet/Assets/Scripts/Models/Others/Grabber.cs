24ec6aa900bfcb787bc6ea957b7d81c0c295d23b
Ari Punavuori <ari.punavuori@gmail.com>
2018-12-14 11:11:51 +0200

    Battery routine

--- ARPet/Assets/Scripts/Models/Others/Grabber.cs
+++ ARPet/Assets/Scripts/Models/Others/Grabber.cs
@@ -10,37 +10,35 @@ public class Grabber : MonoBehaviour, IDragger
 
     IDraggable currentDrag;
     float dragDistance;
-    public void BreakDrag() {
 
+    public void BreakDrag() {
+        currentDrag = null;
     }
 
 	void Update () {
-        var ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
-        RaycastHit hitInfo;
 
-        if(Physics.Raycast(ray, out hitInfo, maxHitDist, HitLayerMask)) {
-            Debug.Log(hitInfo.collider.name);
-            var draggable = hitInfo.collider.GetComponent<IDraggable>();
-            if(draggable != null) {
-                if(Input.GetMouseButtonDown(0)) {
-                    dragDistance = hitInfo.distance;
-                    currentDrag = draggable;
-                    currentDrag.OnDragStart(this, transform.rotation);
+        if(currentDrag != null) {
+            if(Input.GetMouseButtonUp(0)) {
+                currentDrag.OnDragEnd();
+                currentDrag = null;
+            } else {
+                currentDrag.OnDragContinue(transform.position + transform.forward * dragDistance, transform.rotation);
+            }
+        } else {
+            var ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(ray, out hitInfo, maxHitDist, HitLayerMask)) {
+                //Debug.Log(hitInfo.collider.name);
+                var draggable = hitInfo.collider.GetComponent<IDraggable>();
+                if(draggable != null) {
+                    if(Input.GetMouseButtonDown(0)) {
+                        dragDistance = hitInfo.distance;
+                        currentDrag = draggable;
+                        currentDrag.OnDragStart(this, transform.rotation);
+                    }
                 }
             }
         }
-
-        if(currentDrag != null) {
-            currentDrag.OnDragContinue(transform.position + transform.forward * dragDistance, transform.rotation);
-        }
-
-        if(currentDrag !=null && Input.GetMouseButtonUp(0)) {
-            
-            currentDrag.OnDragEnd();
-            currentDrag = null;
-        }
     }
-
-
-
 }
