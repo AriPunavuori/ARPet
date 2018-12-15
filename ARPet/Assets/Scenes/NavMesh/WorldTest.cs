0eadd28bc7c7c72190069ec240d8ec571e531452
Mathias Mustonen <Mathias.Mustonen@gmail.com>
2018-12-15 12:45:26 +0200

    Foo

---
+++ ARPet/Assets/Scenes/NavMesh/WorldTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldTest : MonoBehaviour
+{
+    private NavMeshSurface navMeshSurface;
+    private NavMeshTestController NavMeshTestController;
+
+    private void Awake()
+    {
+        navMeshSurface = GetComponent<NavMeshSurface>();
+    }
+
+    public void Init(NavMeshTestController navMeshTestController)
+    {
+        NavMeshTestController = navMeshTestController;
+    }
+
+    private void Start()
+    {
+        navMeshSurface.BuildNavMesh();
+
+        NavMeshTestController.CreatePlayer();
+    }
+}
